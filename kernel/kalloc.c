c7bd3e62f0a34d450de3d6d3b241b0e8a275d665
WillyOwl <willy@Willy.localdomain>
2025-07-06 23:07:00 +0800

    Function to implement freepage in exercise of chapter2

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,24 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+int freepages(){
+  int count = 0;
+  struct run* r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+
+  while (r != 0){
+    count++;
+    r = r->next;
+  }
+
+  release(&kmem.lock);
+
+  return count;
+}
+
+uint64 sys_kfreemem(){
+  return freepages();
+}
