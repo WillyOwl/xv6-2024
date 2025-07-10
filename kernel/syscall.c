5a564ca76909dc8230fe5815840e4d0833055696
WillyOwl <willy@Willy.localdomain>
2025-07-10 14:55:47 +0800

    Add entry for sys_kfreemem for execrise in chapter 2

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -101,6 +101,9 @@ extern uint64 sys_unlink(void);
 extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
+extern uint64 sys_kfreemem(void);
+
+//Add entry for sys_kfreemem for the interest of exercise in chapter 2
 
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
@@ -126,6 +129,10 @@ static uint64 (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_kfreemem]    sys_kfreemem,
+
+//add an entry for sys_kmem to the syscalls array
+
 };
 
 void
